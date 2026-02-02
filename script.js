79d737be521b39ef863f9b42936e1aa6e11ff8ef
dearraph <danendraaphy@gmail.com>
2026-02-02 23:24:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 function cafeApp() {
-  return {
-    menuList: [],
-    async init() {
-      try {
-        const response = await fetch(
-          "https://api.github.com/repos/dearraph/cafe/contents/data/menu",
-        );
-        const files = await response.json();
-
-        for (const file of files) {
-          if (file.name.endsWith(".json")) {
-            const contentReq = await fetch(file.download_url);
-            const item = await contentReq.json();
-            this.menuList.push(item);
-          }
+    return {
+        menuList: [],
+        async init() {
+            try {
+                const response = await fetch('https://api.github.com/repos/dearraph/cafe/contents/data/menu');
+                const files = await response.json();
+                
+                this.menuList = []; 
+                
+                
+                for (const file of files) {
+                    if (file.name.endsWith('.json')) {
+                        const contentReq = await fetch(file.download_url);
+                        const item = await contentReq.json();
+                        this.menuList.push(item);
+                    }
+                }
+            } catch (error) {
+                console.error("Menu gagal dimuat:", error);
+            }
         }
-      } catch (error) {
-        console.error("Gagal memuat menu:", error);
-      }
-    },
-  };
+    }
 }
